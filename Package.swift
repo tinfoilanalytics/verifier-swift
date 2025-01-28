1718d2bde8c63f767843a4ef6cfe03f4aa29ff93
Mark @ Germ <mark@germ.network>
2025-01-27 16:04:50 -0800

    siwft format, mock out interface

--- Package.swift
+++ Package.swift
@@ -2,23 +2,23 @@
 import PackageDescription
 
 let package = Package(
-  name: "TinfoilVerifier",
-  platforms: [
-    .iOS(.v17)
-  ],
-  products: [
-    .library(
-      name: "TinfoilVerifier",
-      targets: ["TinfoilVerifier-Swift"])
-  ],
-  targets: [
-    .target(
-      name: "TinfoilVerifier-Swift",
-      dependencies: [
-        "TinfoilVerifier"
-      ]
-    ),
-    .binaryTarget(
-      name: "TinfoilVerifier",
-      path: "TinfoilVerifier.xcframework"),
-  ])
+    name: "TinfoilVerifier",
+    platforms: [
+        .iOS(.v17)
+    ],
+    products: [
+        .library(
+            name: "TinfoilVerifier",
+            targets: ["TinfoilVerifier-Swift"])
+    ],
+    targets: [
+        .target(
+            name: "TinfoilVerifier-Swift",
+            dependencies: [
+                "TinfoilVerifier"
+            ]
+        ),
+        .binaryTarget(
+            name: "TinfoilVerifier",
+            path: "TinfoilVerifier.xcframework"),
+    ])
--- Sources/TinfoilVerifier-Swift/TinfoilVerifier.swift
+++ Sources/TinfoilVerifier-Swift/TinfoilVerifier.swift
@@ -10,15 +10,102 @@ import TinfoilVerifier
 
 ///Main App interface
 
-struct TinfoilVerifier {
-  ///client should store this and have policy for refetch
-  //since this reaches into sigstore/tuf, maybe live with this being
-  //a blocking synch method
-  static func fetchTrustRootStore() async throws -> [Data] {
-    throw TinfoilError.mocking
-  }
+public struct TinfoilVerifier {
+    ///client should store this and have policy for refetch
+    //since this reaches into sigstore/tuf, maybe live with this being
+    //a blocking sync method
+    public static func fetchTrustRoot() async throws -> [Data] {
+        throw TinfoilError.mocking
+    }
+
+    //synchronous variant of the above
+    public static func fetchTrustRoot(
+        completionHandler: @escaping (Result<Data, Error>) -> Void
+    ) {
+        DispatchQueue.global().async {
+            completionHandler(.failure(TinfoilError.mocking))
+        }
+
+    }
+}
+
+extension ClientSecureClient {
+    public func verify(
+        enclaveState: ClientEnclaveState,
+        sigStoreTrustRoot: Data  //cached by the application
+    ) async throws {
+        let (_, eifHash) = try await Github.fetchLatestRelease(repo: repo)
+
+        let sigStoreBundle = try await Github.fetchAttestationBundle(
+            repo: repo,
+            digest: eifHash
+        )
+
+        let codeMeasurements = try SigStore.verifyMeasurementAttestation(
+            trustedRootJSON: sigStoreTrustRoot,
+            bundleJSON: sigStoreBundle,
+            hexDigest: eifHash,
+            repo: repo
+        )
+
+        //since this doesn't rely on the above, seems like we can cache
+        //the validation result
+        let (enclaveAttestation, enclaveCertFP) = try await Enclave.fetch(host: enclave)
+
+        let (enclaveMeasurements, attestedCertFP) = try await enclaveAttestation.verify()
+
+        guard enclaveCertFP == attestedCertFP else {
+            throw TinfoilError.mismatchedCertificates
+        }
+
+        guard enclaveMeasurements == codeMeasurements else {
+            throw TinfoilError.mismatchedMeasurements
+        }
+
+        //mutate and return verified state
+
+        //        return verifiedState
+    }
 }
 
 enum TinfoilError: Error {
-  case mocking
+    case mocking
+    case mismatchedCertificates
+    case mismatchedMeasurements
+}
+
+//TODO: for Tinfoil
+//stubbing interface from the golang function interface that may be missing
+extension ClientSecureClient {
+    var repo: String { "inference-enclave.tinfoil.sh" }
+    var enclave: String { "tinfoilanalytics/nitro-enclave-build-demo" }
+
+    //
+}
+
+struct SigStore {
+    static func verifyMeasurementAttestation(
+        trustedRootJSON: Data,
+        bundleJSON: Data,
+        hexDigest: String,
+        repo: String
+    ) throws -> Measurement? {
+        return nil
+    }
+}
+
+struct Measurement: Equatable {
+
+}
+
+struct Enclave {
+    static func fetch(host: String) async throws -> (Document, Data) {
+        throw TinfoilError.mocking
+    }
+}
+
+struct Document {
+    func verify() throws -> (Measurement, Data) {
+        throw TinfoilError.mocking
+    }
 }
